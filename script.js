854bb724cf7d12f534f512deec258ea3ecfc6b1c
NaveenYadav <yn.naveen00@gmail.com>
2023-12-15 20:34:16 +0530

    4.0.0

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const n = 20;
 const array = [];
+const array_for_selection = [];
 init();
+init_for_selection();
 
 function init() {
     for (let i = 0; i < n; i++) {
@@ -9,13 +11,13 @@ function init() {
     showBars();
 }
 
+
 function bubbleSort(arr) {
     const moves = [];
     let len = arr.length;
     let isSwapped = false;
     for (let i = 0; i < len; i++) {
         isSwapped = false;
-
         for (let j = 0; j < (len - i - 1); j++) {
             if (arr[j] > arr[j + 1]) {
                 moves.push([j, j + 1]);
@@ -25,7 +27,7 @@ function bubbleSort(arr) {
                 isSwapped = true;
             }
         }
-        if (!isSwapped) {
+        if (isSwapped == false) {
             break;
         }
     }
@@ -63,13 +65,113 @@ function showBars(move) {
     for (let i = 0; i < array.length; i++) {
         const bar = document.createElement('div');
         bar.style.height = array[i] * 100 + "%";
-        bar.classList.add("bar");
+        bar.classList.add("barForBubble");
         if (move && move.includes(i)) {
             bar.style.backgroundColor = "red";
         }
         barGraph.appendChild(bar);
     }
 }
+
+
+
+
+//Selection Sort
+//Time Complexity -O(n^2)
+
+function init_for_selection() {
+    for (let i = 0; i < n; i++) {
+        array_for_selection[i] = Math.random();
+    }
+    showBars_for_selection();
+}
+
+function sort_by_selection() {
+    const copy_for_selection = [...array_for_selection]
+    const moves_for_selection = selectionSort(copy_for_selection);
+    animate_for_selection(moves_for_selection);
+}
+
+function animate_for_selection(moves_for_selection) {
+    if (moves_for_selection.length == 0) {
+        showBars_for_selection();
+        return;
+    }
+    const SelectionMove = moves_for_selection.shift();
+    const k = SelectionMove[0];
+    const l = SelectionMove[1];
+    [array_for_selection[k], array_for_selection[l]] = [array_for_selection[l], array_for_selection[k]];
+    showBars_for_selection(SelectionMove);
+    setTimeout(function () {
+        animate_for_selection(moves_for_selection);
+    }, 200);
+}
+
+function showBars_for_selection(SelectionMove) {
+    selection.innerHTML = "";
+    for (let i = 0; i < array_for_selection.length; i++) {
+        const bar1 = document.createElement('div');
+        bar1.style.height = array_for_selection[i] * 100 + "%";
+        bar1.classList.add("barForSelection");
+        if (SelectionMove && SelectionMove.includes(i)) {
+            bar1.style.backgroundColor = "red";
+        }
+        selection.appendChild(bar1);
+    }
+}
+
+function selectionSort(arr1) {
+    const moves = [];
+    const n = arr1.length;
+    let min_idx;
+    for (let i = 0; i < n - 1; i++) {
+        min_idx = i;
+        for (let j = i + 1; j < n; j++) {
+            if (arr1[j] < arr1[min_idx])
+                min_idx = j;
+        }
+        moves.push([i, min_idx]);
+        let temp = arr1[min_idx];
+        arr1[min_idx] = arr1[i];
+        arr1[i] = temp;
+    }
+    return moves;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Insertion Sort
 //Time Complexity -O(n^2)
@@ -80,10 +182,6 @@ function insertionSort(arr) {
     for (let i = 1; i < n; i++) {
         key = arr[i];
         j = i - 1;
-
-        /* Move elements of arr[0..i-1], that are  
-        greater than key, to one position ahead  
-        of their current position */
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
             j = j - 1;
@@ -91,33 +189,6 @@ function insertionSort(arr) {
         arr[j + 1] = key;
     }
 }
-
-//Selection Sort
-//Time Complexity -O(n^2)
-
-function selectionSort(arr, n) {
-    let min_idx;
-    for (let i = 0; i < n - 1; i++) {
-        min_idx = i;
-        for (let j = i + 1; j < n; j++) {
-            if (arr[j] < arr[min_idx])
-                min_idx = j;
-        }
-        let temp = arr[min_idx];
-        arr[min_idx] = arr[i];
-        arr[i] = temp;
-    }
-}
-
-
-
-
-
-
-
-
-
-
 
 
 
